2f6ff93d761bde371ac2682b2900b86a427d19c7
echeverriaalex <aleex.naahuel@outlook.com>
2022-09-13 00:29:51 -0300

    creo la estructura basica del proyecto

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = require('./router');
+const app = express();
+const port = 3000;
+
+app.use('/', router);
+
+app.listen(port, ()=>{
+    console.log(`Segundo entregable is working on port ${port}`);
+});
