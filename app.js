eac85f19805746e2d0d58df5e2a004b72872863c
echeverriaalex <aleex.naahuel@outlook.com>
2022-10-10 22:19:30 -0300

    elimino lo que no uso

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = require('./router');
-require("dotenv").config()
 const app = express();
 const port = 3000;
-const Sequelize = require("sequelize");
 app.use('/', router);
 
 app.listen(port, ()=>{
